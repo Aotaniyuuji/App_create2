d5abac695a5596115e1cfc856bd392757d9dcd60
EC2 Default User <129158258+Aotaniyuuji@users.noreply.github.com>
2023-08-08 16:10:13 +0000

    [update]答え合わせ

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -3,44 +3,44 @@
 require 'rails_helper'
 
 describe '投稿のテスト' do
-  let! visit root_path
+  let!(:list) { create(:list, title: 'hoge', body: 'body') }
   describe 'トップ画面(top_path)のテスト' do
     before 'トップ画面への遷移' do
       visit top_path
     end
     context '表示の確認' do
       it 'トップ画面(top_path)に「ここはTopページです」が表示されているか' do
-        expect(page).to heve_content('ここはtopページです')
+        expect(page).to heve_content 'ここはTopページです'
       end
       it 'top_pathが"/top"であるか' do
-        expect(current_path).to eq '/top'
+        expect(current_path).to eq('/top')
       end
     end
   end
-end
 
-describe '投稿画面のテスト' do
-  before '投稿画面への遷移' do
-    visit new_list_path
-  end
-  context '表示の確認' do
-    it 'new_list_pathが"/lists/newであるか"' do
-      expect(current_path).to eq('/lists/new')
+  describe '投稿画面(new_lsit_path)のテスト' do
+    before '投稿画面への遷移' do
+      visit new_list_path
     end
-    it '投稿ボタンが表示されているか' do
-      expect(page).to have_button '投稿'
+    context '表示の確認' do
+      it 'new_list_pathが"/lists/newであるか"' do
+        expect(current_path).to eq('/lists/new')
+      end
+      it '投稿ボタンが表示されているか' do
+        expect(page).to have_button '投稿'
+      end
     end
-  end
-  context '投稿処理のテスト' do
-    it '投稿後のリダイレクト先は正しいか' do
-      fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
-      fill_in 'list[body]', with: Faker::Lorem.characters(number:30)
-      click_button 'Create List'
-      expect(page).to have_current_path list_path(List.last)
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number:30)
+        click_button '投稿'
+        expect(page).to have_current_path list_path(List.last)
+      end
     end
   end
 
-  describe '一覧画面のテスト' do
+  describe '投稿一覧のテスト' do
     before '一覧画面への遷移' do
       visit lists_path
     end
@@ -54,36 +54,38 @@ describe '投稿画面のテスト' do
 
   describe '詳細画面のテスト' do
     before '詳細画面への確認' do
-      visit show_path
+      visit list_path(list)
     end
     context '表示の確認' do
       it '削除リンクが存在しているか' do
-        expect(page).to have_link '削除リンク'
+        expect(page).to have_link '削除'
       end
       it '編集リンクが存在しているか' do
-        expect(page).to have_link '編集リンク'
+        expect(page).to have_link '編集'
       end
     end
     context 'リンクの遷移先の確認' do
       it '編集の遷移先は編集画面か' do
-        edit_link = find_all()
-        expect(edit_link.native.inner_text).to include('/edit')
+        edit_link = find_all('a')[3]
+        edit_link.click
+        expect(current_path).to eq('/lists/' + list.id.to_s + '/edit')
       end
     end
     context 'list削除のテスト' do
       it 'listの削除' do
-        expect { list.destroy }.to change{ Book.count }.by(-1)
+        expect { list.destroy }.to change{ List.count }.by(-1)
       end
     end
   end
 
-  dexcribe '編集画面のテスト' do
+  describe '編集画面のテスト' do
     before '編集画面への遷移' do
-      visit edit_path
+      visit edit_list_path(list)
     end
     context '表示の確認' do
       it '編集前のタイトルと本文がフォームに表示(セット)されている' do
-        expect()
+        expect(page).to have_field 'list[title]', with: list.title
+        expect(page).to have_field 'list[body]', with: list.body
       end
       it '保存ボタンが表示される' do
         expect(page).to have_button '保存'
@@ -93,7 +95,7 @@ describe '投稿画面のテスト' do
       it '更新後のリダイレクト先は正しいか' do
         fill_in 'list[title]', with: Faker::Lorem.characters(number:10)
         fill_in 'list[body]', with: Faker::Lorem.characters(number:30)
-        click_button 'Update List'
+        click_button '保存'
         expect(page).to have_current_path list_path(list)
       end
     end
